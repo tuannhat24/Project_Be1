3e872aead30e5b657b863c3fddb4833f3fe14df3
tuannhat24 <tuannhat124@gmail.com>
2024-12-18 11:36:53 +0700

    fix css

--- Booking-movies/includes/header.php
+++ Booking-movies/includes/header.php
@@ -32,6 +32,7 @@ if (isset($_SESSION['user_id'])) {
     <title>Booking Movies</title>
     <link rel="stylesheet" href="/Project_Be1/Booking-movies/assets/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
